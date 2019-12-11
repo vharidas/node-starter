91163578e3c293f86de3a85aa9562fc787642443
vharidas <vivek.haridas+github@gmail.com>
2019-12-11 21:21:53 +0530

    using GunZip instead of UnZip

--- pass.js
+++ pass.js
@@ -1,6 +1,6 @@
 const zlib = require('zlib');
 const { stdin, stdout } = process;
-const transforms = { zip: zlib.createGzip(), unzip: zlib.createUnzip() };
+const transforms = { zip: zlib.createGzip(), unzip: zlib.createGunzip() };
 const usage = `node pass.js [ ${Object.keys(transforms).join(' | ')} ] < from_file > to_file`;
 const main = () => {
     const [, , name] = process.argv;
