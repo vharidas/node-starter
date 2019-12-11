e1d90f6e3141753e3d1db755b0d5e65456ac77e4
vharidas <vivek.haridas+github@gmail.com>
2019-12-11 20:21:02 +0530

    pass through contents zipped or unizpped

---
+++ pass.js
@@ -0,0 +1,11 @@
+const zlib = require('zlib');
+const { stdin, stdout } = process;
+const transforms = { zip: zlib.createGzip(), unzip: zlib.createUnzip() };
+const usage = `node pass.js [ ${Object.keys(transforms).join(' | ')} ] < from_file > to_file`;
+const main = () => {
+    const [, , name] = process.argv;
+    const transform = transforms[name];
+    if (transform) stdin.pipe(transform).pipe(stdout);
+    else console.error(usage);
+}
+main();
