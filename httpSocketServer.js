892a76ccca202fc4b2b431c42ff0795cfa8dde8e
vharidas <vivek.haridas+github@gmail.com>
2020-01-23 14:21:20 +0530

    introduced query,body parsing & post method

--- httpSocketServer.js
+++ httpSocketServer.js
@@ -1,23 +1,53 @@
 const { Server } = require('net');
 const fs = require('fs');
+const pickupParams = (query,keyValue)=>{
+  const [key,value] = keyValue.split('=');
+  query[key] = value;
+  return query;
+};
+const readParams = keyValueTextPairs => keyValueTextPairs.split('&').reduce(pickupParams,{});
+const parseQueryParameters = entireUrl =>{
+  const [url,queryText] = entireUrl.split('?');
+  const query = queryText && readParams(queryText);
+  return {url,query};
+}
+const collectHeadersAndContent = (result, line) => {
+  if (line === '') {
+    result.body = '';
+    return result;
+  }
+  if ('body' in result) {
+    result.body += line;
+    return result;
+  }
+  const [key, value] = line.split(': ');
+  result.headers[key] = value;
+  return result;
+};
 class Request {
-  constructor(method, url, headers, body) {
+  constructor(method, url, query, headers, body) {
     this.method = method;
     this.url = url;
+    this.query = query;
     this.headers = headers;
     this.body = body;
   }
   static parse(requestText) {
     const [requestLine, ...headersAndBody] = requestText.split('\r\n');
-    const [method, url, protocol] = requestLine.split(' ');
-    const { headers, body } = headersAndBody.reduce(collectHeadersAndContent, { headers: {} });
-    const req =  new Request(method, url, headers, body);
+    const [method, entireUrl, protocol] = requestLine.split(' ');
+    const {url,query} = parseQueryParameters(entireUrl);
+    let { headers, body } = headersAndBody.reduce(collectHeadersAndContent, { headers: {} });
+    if(headers['Content-Type'] === 'application/x-www-form-urlencoded'){
+      body = readParams(body);
+    }
+    const req =  new Request(method, url, query, headers, body);
     console.warn(req);
     return req;
   }
 }
 const STATIC_FOLDER = `${__dirname}/public`;
 const CONTENT_TYPES = {
+  txt: 'text/plain',
   html: 'text/html',
   css: 'text/css',
   js: 'application/javascript',
@@ -26,20 +56,6 @@ const CONTENT_TYPES = {
 };
 
 let visitorCount = 0;
-const collectHeadersAndContent = (result, line) => {
-  if (line === '') {
-    result.content = '';
-    return result;
-  }
-  if ('content' in result) {
-    result.content += line;
-    return result;
-  }
-  const [key, value] = line.split(': ');
-  result.headers[key] = value;
-  return result;
-};
-
 class Response {
   constructor() {
     this.statusCode = 404;
@@ -81,10 +97,46 @@ const serveHomePage = (req) => {
   <html>
     <head><title>The Socket Server</title></head>
     <body>
+      <script>
+        const sendHttpGET = (url,callback)=>{
+          const req = new XMLHttpRequest();
+          req.onload = function(){
+            if(this.status===200) callback(this.responseText);
+          }
+          req.open("GET",url);
+          req.send();
+        }
+        const reloadCount = ()=>{
+          sendHttpGET("table.html",(text)=>{
+            document.querySelector('footer #count').innerHTML = text;
+          });
+        }
+      </script>
       <p>Welcome to the Socket Server</p>
       <img src="smiley.gif"></img>
       <p>Thanks for asking ${req.url}</p>
-      <footer>You are visitor ${visitorCount}</footer>
+      <footer onclick="reloadCount()">You are visitor <span id="count">${visitorCount}</span></footer>
+    </body>
+  </html>`;
+  const res = new Response();
+  if(!req.headers['Cookie']) res.setHeader('Set-Cookie',`sessionId=${new Date().getTime()}`);
+  res.setHeader('Content-Type', CONTENT_TYPES.html);
+  res.setHeader('Content-Length', html.length);
+  res.statusCode = 200;
+  res.body = html;
+  return res;
+}
+
+const registerStudent = (req)=>{
+  const student = req.query;
+  const html = `
+  <html>
+    <head><title>The Socket Server</title></head>
+    <body>
+      
+      <p>Thanks for Registering</p>
+      <p>You will be known as ${student.firstName} ${student.lastName}</p>
+      <footer>You will be studying in class ${student.standard}</footer>
     </body>
   </html>`;
   const res = new Response();
@@ -94,8 +146,39 @@ const serveHomePage = (req) => {
   res.body = html;
   return res;
 }
+const registerStudentPost = (req)=>{
+  const student = req.body;
+  const html = `
+  <html>
+    <head><title>The Socket Server</title></head>
+    <body>
+      
+      <p>Thanks for Registering</p>
+      <p>You will be known as ${student.firstName} ${student.lastName}</p>
+      <footer>You will be studying in class ${student.standard}</footer>
+    </body>
+  </html>`;
+  const res = new Response();
+  res.setHeader('Content-Type', CONTENT_TYPES.html);
+  res.setHeader('Content-Length', html.length);
+  res.statusCode = 200;
+  res.body = html;
+  return res;
+}
+const serveVisitorCount = (req)=>{
+  const text = `${visitorCount}`;
+  const res = new Response();
+  res.setHeader('Content-Type', CONTENT_TYPES.txt);
+  res.setHeader('Content-Length', text.length);
+  res.statusCode = 200;
+  res.body = text;
+  return res;
+}
 const findHandler = (req) => {
   if(req.method === 'GET' && req.url==='/') return serveHomePage;
+  if(req.method === 'GET' && req.url==='/visitorCount') return serveVisitorCount;
+  if(req.method === 'GET' && req.url === '/registerStudent') return registerStudent;
+  if(req.method === 'POST' && req.url === '/registerStudent') return registerStudentPost;
   if(req.method === 'GET') return serveStaticFile;
   return ()=>new Response();
 }
